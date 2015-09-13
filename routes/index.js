558e21b201dfe1e9f796c9caa49ad475863d18c4
Saurav Mohapatra <saurav_mohapatra@outlook.com>
2015-09-12 23:19:05 -0400

    Changed index URL

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile("home.html");
+  res.sendFile("../views/home.html");
 });
 
 module.exports = router;
