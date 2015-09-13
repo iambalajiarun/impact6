10f837adae0942754950066d033ca11d754f8ab3
Saurav Mohapatra <saurav_mohapatra@outlook.com>
2015-09-12 23:24:32 -0400

    Changed static location

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var find = require('./routes/find');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public/views')));
 
 app.use('/', routes);
 app.use('/find', find);
